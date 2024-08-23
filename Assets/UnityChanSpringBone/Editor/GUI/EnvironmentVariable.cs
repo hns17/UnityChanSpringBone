34928c41c79e9282b0ae872421f7d73034881f05
hns17 <hns17@naver.com>
2024-08-23 21:17:05 +0900

    ucsb localization

---
+++ Assets/UnityChanSpringBone/Editor/GUI/EnvironmentVariable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using Unity.Animations.SpringBones;
+using UnityEngine;
+
+public static class EnvironmentVariable
+{
+    public const string menuPath = "Tools/SpringBone";
+}
